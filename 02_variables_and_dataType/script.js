dd80cd2313114f2036d3537869ff0d2c387104a3
Éric Ivan João António <ericantoniodev@gmail.com>
2022-09-27 14:52:57 +0200

    Update script.js

--- 02_variables_and_dataType/script.js
+++ 02_variables_and_dataType/script.js
@@ -11,3 +11,8 @@ const ano = 2022
 
 var dia = 27
 console.log(provincia+ ' aos ' + dia + ' de ' + ano);
+
+// Variable Naming
+// 1- The should be unique!
+// 2- The name should not be reserved Keyword!
+// 3- the name must start with a character , _ or $!
