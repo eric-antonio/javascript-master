a608cdf703f2126aac03f3d6b7bb86f13c0a2e83
Éric Ivan João António <ericantoniodev@gmail.com>
2022-09-27 15:07:26 +0200

    Numbers
    // Numbers
    let valor1 = "2"
    let valor2 = 3
    console.log(` A soam de ${valor1} + ${valor2} = ${Number(valor1) + valor2}`)

--- 02_variables_and_dataType/script.js
+++ 02_variables_and_dataType/script.js
@@ -4,6 +4,6 @@ let backits = `Hello, ${nome}, Welcome!`;
 console.log( typeof backits);
 
 // Numbers
-let valor1 = 323
-let valor2 = 34234
-console.log(` A soam de ${valor1} + ${valor2} = ${valor1 + valor2}`)
+let valor1 = "2"
+let valor2 = 3
+console.log(` A soam de ${valor1} + ${valor2} = ${Number(valor1) + valor2}`)
