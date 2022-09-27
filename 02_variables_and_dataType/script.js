f8c56077fdf4f8dd868f29342a696ff7ddd68c03
Éric Ivan João António <ericantoniodev@gmail.com>
2022-09-27 15:00:27 +0200

    Update script.js

--- 02_variables_and_dataType/script.js
+++ 02_variables_and_dataType/script.js
@@ -1,18 +1,6 @@
-//Creating a variable usuing var
-var mes = 'Stembro!'
+// String
+let nome ="Eric Antonio!";
+let backits = `Hello, ${nome}!`;
 
-//Creating a variable usuing let
-let  provincia = 'Maputo'
 
-//Creating a variable usuing Const
-const ano = 2022
-
-//Creating a variable usuing var
-
-var dia = 27
-console.log(provincia+ ' aos ' + dia + ' de ' + ano);
-
-// Variable Naming
-// 1- The should be unique!
-// 2- The name should not be reserved Keyword!
-// 3- the name must start with a character , _ or $!
+console.log(backits);
