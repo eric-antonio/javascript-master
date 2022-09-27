7950d0f5d0cf3ae1f3245e6b52a7803620e9ca2e
Éric Ivan João António <ericantoniodev@gmail.com>
2022-09-27 15:04:35 +0200

    Update script.js

--- 02_variables_and_dataType/script.js
+++ 02_variables_and_dataType/script.js
@@ -1,6 +1,9 @@
 // String
 let nome ="Eric Antonio!";
-let backits = `Hello, ${nome}!`;
+let backits = `Hello, ${nome}, Welcome!`;
+console.log( typeof backits);
 
-
-console.log(backits);
+// Numbers
+let valor1 = 323
+let valor2 = 34234
+console.log(` A soam de ${valor1} + ${valor2} = ${valor1 + valor2}`)
